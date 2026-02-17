c4203d8db2d3ee37aaf33fce73c4a6e091beae70
Andrey <argadron@bk.ru>
2026-02-17 16:19:26 +0500

    chore: remake tests

--- test/collision.test.ts
+++ test/collision.test.ts
@@ -1,11 +1,68 @@
-import createGame from "@"
+import type { Game } from "@"
+import { createGame } from "@"
 import { GameObjectEnum } from "@enums"
-import type { IEntityCreatedCollisionData, IMovedCollisionData, IObjectCreatedCollisionData } from "@interfaces"
+import type { 
+    IEntityCreatedCollisionData, 
+    IMovedCollisionData, 
+    IObjectCreatedCollisionData,
+    IEntityManager,
+    IGameMap
+} from "@interfaces"
 import { checkTwoPositions } from "@utils"
 import type { Position } from "@types"
+import type { Entity, GameObject } from "@world"
 
 describe('Collisions Tests', () => {
-    const [game, manager, map] = createGame()
+    let game!: Game
+    let manager!: IEntityManager
+    let map!: IGameMap
+
+    let player!: Entity
+    let zombie!: Entity
+    let wall!: GameObject
+
+    beforeEach(() => {
+        const [g, m, mapInstance] = createGame()
+
+        game = g
+        manager = m
+        map = mapInstance
+
+        player = manager.create({
+            name: PLAYER,
+            health: 10,
+            damage: 10,
+            isDead: false,
+            position: [1, 0]
+        })
+
+        zombie = manager.create({
+            name: ZOMBIE,
+            health: 10,
+            damage: 10,
+            isDead: false,
+            position: [1, 1]
+        })
+
+        wall = map.createObject({
+            name: WALL,
+            position: [0, 1],
+            type: GameObjectEnum.WALL
+        })
+
+        game.on<IMovedCollisionData>('entityMovedCollision', (o, e, d) => {
+            const { collisionPosition  } = d.eventData
+
+            if (checkTwoPositions(player.position, collisionPosition as Position)) collisionMoveToEntityTest = true
+            if (checkTwoPositions(player.position, collisionPosition as Position)) collisionMoveToObjectTest = true
+        })
+        game.on<IEntityCreatedCollisionData>('entityCreatedCollision', (o, e, d) => {
+            collisionCreateTest = true
+        })
+        game.on<IObjectCreatedCollisionData>('objectCreatedCollision', (o, e, d) => {
+            collisionCreateObjectTest = true
+        })
+    })
 
     const PLAYER = 'PLAYER'
     const ZOMBIE = 'ZOMBIE'
@@ -16,40 +73,7 @@ describe('Collisions Tests', () => {
     let collisionMoveToEntityTest = false
     let collisionMoveToObjectTest = false
 
-    const player = manager.create({
-        name: PLAYER,
-        health: 10,
-        damage: 10,
-        isDead: false,
-        position: [1, 0]
-    })
-
-    const zombie = manager.create({
-        name: ZOMBIE,
-        health: 10,
-        damage: 10,
-        isDead: false,
-        position: [1, 1]
-    })
-
-    const wall = map.createObject({
-        name: WALL,
-        position: [0, 1],
-        type: GameObjectEnum.WALL
-    })
-
-    game.on<IMovedCollisionData>('entityMovedCollision', (o, e, d) => {
-        const { collisionPosition  } = d.eventData
-
-        if (checkTwoPositions(player.position, collisionPosition as Position)) collisionMoveToEntityTest = true
-        if (checkTwoPositions(player.position, collisionPosition as Position)) collisionMoveToObjectTest = true
-    })
-    game.on<IEntityCreatedCollisionData>('entityCreatedCollision', (o, e, d) => {
-        collisionCreateTest = true
-    })
-    game.on<IObjectCreatedCollisionData>('objectCreatedCollision', (o, e, d) => {
-        collisionCreateObjectTest = true
-    })
+    
 
     it('Create entity in collision place', () => {
         manager.create({

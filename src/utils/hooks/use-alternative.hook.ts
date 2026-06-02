34caa1755ae59b8abeffd0b5ac83c098e1751e0c
Andrey <argadron@bk.ru>
2026-06-02 17:31:09 +0500

    feat: add array support in useAlternative

--- src/utils/hooks/use-alternative.hook.ts
+++ src/utils/hooks/use-alternative.hook.ts
@@ -5,17 +5,18 @@ import { AsyncPlugin } from "@plugins";
 /**
  * This hook allows you to client-side prediction command
  * Command will be executed in alternative game
- * @param command - Command to execute in alternative world
+ * @param command - Command(s) to execute in alternative world
  * @param core - Game reference if hydration disabled
  * @returns { Promise<Game> } - Alternative isolated game (not affect real)
  */
-export async function useAlternative(command: ICommand, core?: Game): Promise<Game> {
+export async function useAlternative<T = any>(command: ICommand<T> | ICommand<T>[], core?: Game): Promise<Game> {
     const game = useAlternative.prototype.game as Game || core
 
     const asyncPlugin = new AsyncPlugin()
     const alternative = Game.fromSnapshot(game.save(), alternative => alternative.registerPlugin(asyncPlugin))
 
-    await asyncPlugin.asyncExecute(command)
+    if (!Array.isArray(command)) await asyncPlugin.asyncExecute(command)
+    else for (const cmd of command) await asyncPlugin.asyncExecute(cmd)
 
     return alternative
 }
